a1622751a00546fc36f3a2d4671f8689d5761769
sheyennevalley <jstrittmatter@sheyennevalley.com>
2015-11-01 16:52:31 -0600

    added unified client

---
+++ src/main/java/com/sheyennevalley/rancher/v1/RancherClient.java
@@ -0,0 +1,39 @@
+package com.sheyennevalley.rancher.v1;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sheyennevalley.rancher.v1.containers.ContainersClient;
+import com.sheyennevalley.rancher.v1.containers.ContainersRancherClient;
+import com.sheyennevalley.rancher.v1.containers.model.Container;
+import com.sheyennevalley.rancher.v1.services.ServicesClient;
+import com.sheyennevalley.rancher.v1.services.ServicesRancherClient;
+import com.sheyennevalley.rancher.v1.services.model.Service;
+
+import java.util.List;
+
+/**
+ * Created by justin on 11/1/15.
+ */
+public class RancherClient implements ServicesClient, ContainersClient {
+
+    private final ServicesClient servicesClient;
+    private final ContainersClient containersClient;
+
+    public RancherClient(BaseClient baseClient, ObjectMapper objectMapper){
+        servicesClient = new ServicesRancherClient(baseClient, objectMapper);
+        containersClient = new ContainersRancherClient(baseClient, objectMapper);
+    }
+
+    public RancherClient(){
+        this(new BaseClient(), new ObjectMapper());
+    }
+
+    @Override
+    public Response<List<Container>> getContainers() {
+        return containersClient.getContainers();
+    }
+
+    @Override
+    public Response<List<Service>> getServices() {
+        return servicesClient.getServices();
+    }
+}
